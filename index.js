8f165238cf213b114de6c3b21903b97222ae9ee1
chetan <chetanchoudhary435@gmail.com>
2026-01-29 17:58:56 +0530

    basic folder structure

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, Nimbus 2k26 Backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}); 
